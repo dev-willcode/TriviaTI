c47b97332130705eac095aae18e979097f40ed6f
Willy Carreño <dev.willct@gmail.com>
2022-06-10 17:26:12 -0500

    winner/looser fragment

--- app/src/main/java/dev/willcode/triviati/MainActivity.kt
+++ app/src/main/java/dev/willcode/triviati/MainActivity.kt
@@ -1,11 +1,19 @@
 package dev.willcode.triviati
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import dev.willcode.triviati.ui.home.infraestructure.HomeFragmentDirections
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.content_main)
+    }
+
+    fun playGame(view: View) {
+        val action = HomeFragmentDirections.openQuiz()
+        view.findNavController().navigate(action)
     }
 }
